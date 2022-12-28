65ade1506257b6b1108028a5e06098ad913d6dfc
Mahir GÜL <mahirgul@gmail.com>
2022-12-28 23:00:14 +0300

    Update googletts.php

--- googletts.php
+++ googletts.php
@@ -58,6 +58,7 @@ function saveSound()
 		$file_name = strtolower(md5(uniqid($text)) . '.mp3');
 		if(file_put_contents($file_name, base64_decode($speech_data)))
 		{
+			//mp3 file convert to wav
 			shell_exec("ffmpeg -i ".$file_name." -ac 1 -ab 128k -ar 8000 -acodec pcm_s16le ".$file_name.".wav");
 			shell_exec("chmod 777 ".$file_name.".wav");
 
