8fdcb849c33326cff8311e8beac71fc1fa0009e2
Mahir GÜL <mahirgul@gmail.com>
2022-12-29 19:44:45 +0300

    Update googletts.php

--- googletts.php
+++ googletts.php
@@ -4,16 +4,20 @@ class GoogleTTS
 {
 	function getSound($text, $apikey, $lang, $voiceName, $folder)
 	{
+		//get md5 hash filename
 		$file_name = $folder.strtolower(md5($text));
 		
 		if(file_exists("$file_name.wav"))
 		{
+			//if file is exist return filename
 			return $file_name;
 		}
 		else
 		{
+			//if file is not exist start process
 			$text = trim($text);
 			if($text == '') return false;
+			//Create request text
 			$params = [
 				"audioConfig"=>[
 					"audioEncoding"=>"LINEAR16",
@@ -36,7 +40,7 @@ class GoogleTTS
 
 			$url = "https://texttospeech.googleapis.com/v1/text:synthesize?fields=audioContent&key=$apikey";
 			$handle = curl_init($url);
-			
+			//Create curl text			
 			curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "POST"); 
 			curl_setopt($handle, CURLOPT_POSTFIELDS, $data_string);  
 			curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
@@ -45,22 +49,29 @@ class GoogleTTS
 				'Content-Length: ' . strlen($data_string)
 				]                                                                       
 			);
+			//run curl request
 			$response = curl_exec($handle);              
 			$responseDecoded = json_decode($response, true);  
 			curl_close($handle);
+			
+			//if curl request getting back the data
 			if($responseDecoded['audioContent'])
 			{
-				$speech_data = $responseDecoded['audioContent'];
+				$speech_data = $responseDecoded['audioContent'];				
 				
-				
-					if(file_put_contents("$file_name.mp3", base64_decode($speech_data)))
-					{
-						shell_exec("ffmpeg -i $file_name.mp3 -ac 1 -ab 128k -ar 8000 -acodec pcm_s16le $file_name.wav");
-						shell_exec("chmod 777 .$file_name.wav");
-						shell_exec("rm $file_name.mp3");
-						return $file_name;
-					}			
-				
+				if(file_put_contents("$file_name.mp3", base64_decode($speech_data)))
+				{
+					//Decode data and save file
+					//mp3 file convert to wav
+					shell_exec("ffmpeg -i $file_name.mp3 -ac 1 -ab 128k -ar 8000 -acodec pcm_s16le $file_name.wav");
+					//set wav file chmod
+					shell_exec("chmod 777 .$file_name.wav");
+					//delete mp3 file
+					shell_exec("rm $file_name.mp3");
+					//return filename
+					return $file_name;
+				}			
+
 			}
 			return null;  
 		}
